2e0cc1147ed9c1924efa02007f640f6f24b983b4
Giampaolo Eusebi <giampaolo.eusebi@gmail.com>
2019-12-05 10:41:26 +0100

    Fix missing exports

--- floatedu/Float.py
+++ floatedu/Float.py
@@ -1,6 +1,8 @@
 
 __all__ = (
-    "Float16", "BFloat16", "Float32", "Float64", "Float128",
+    "Float",
+    "Float8", "Float16", "BFloat16", "Float32",
+    "Float64", "Float128", "Float256",
 )
 
 
--- floatedu/__init__.py
+++ floatedu/__init__.py
@@ -1,3 +1,5 @@
 from .Float import (
-    Float8, Float16, BFloat16, Float32, Float64, Float128, Float256
+    Float,
+    Float8, Float16, BFloat16, Float32,
+    Float64, Float128, Float256
 )
